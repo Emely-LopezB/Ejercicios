1b433e82a5e8817da4dc06aed7ebd5d13c4b45a4
Emely-LopezB <113271158+Emely-LopezB@users.noreply.github.com>
2022-09-11 10:45:09 -0600

    Add files via upload

---
+++ Periodo3Laboratorio1ejercicio3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package periodo3laboratorio1ejercicio3;
+
+/**
+ *
+ * @author Emely Sofia
+ */
+public class Periodo3Laboratorio1ejercicio3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
